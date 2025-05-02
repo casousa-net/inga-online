1b3f060c015756408c974fe642b85a4ee319f161
Orlando Nzagi <suporte@casousa.net>
2025-05-02 21:11:05 +0100

    Corrigir imports relativos para deploy na Vercel

--- app/direccao/dir_relatorios/layout.tsx
+++ app/direccao/dir_relatorios/layout.tsx
@@ -1,5 +1,5 @@
-import '../globals.css';
-import SidebarDireccao from '../components/SidebarDireccao';
+import '../../globals.css';
+import SidebarDireccao from '../../components/SidebarDireccao';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
